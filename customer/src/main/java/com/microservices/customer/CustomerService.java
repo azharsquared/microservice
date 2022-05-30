4013950191022eb39a0cba311f5544d84b9994c3
azharsqaured <azharsquared@gmail.com>
2022-05-30 22:04:20 +0530

    openfeign implemented

--- customer/src/main/java/com/microservices/customer/CustomerService.java
+++ customer/src/main/java/com/microservices/customer/CustomerService.java
@@ -1,6 +1,8 @@
 package com.microservices.customer;
 
+import com.microservices.clients.fraud.FraudClient;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -9,7 +11,8 @@ import org.springframework.web.client.RestTemplate;
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
-    private final RestTemplate restTemplate;
+    private final FraudClient fraudClient;
+    //private final RestTemplate restTemplate;
 
     public void registerCustomer(CustomerRegistrationRequest customerRegistrationRequest) {
         Customer customer = Customer.builder().firstName(customerRegistrationRequest.firstName())
@@ -17,7 +20,7 @@ public class CustomerService {
                 .email(customerRegistrationRequest.email()).build();
 
         customerRepository.saveAndFlush(customer);
-        restTemplate.getForObject("http://FRAUD/api/v1/fraud-check/{customerID}",FraudCheckResponse.class,customer.getId());
-
+       // restTemplate.getForObject("http://FRAUD/api/v1/fraud-check/{customerID}",FraudCheckResponse.class,customer.getId());
+        fraudClient.isFraudster(customer.getId());
     }
 }
--- fraud/src/main/java/com/microservices/fraud/FraudCheckController.java
+++ fraud/src/main/java/com/microservices/fraud/FraudCheckController.java
@@ -1,5 +1,6 @@
 package com.microservices.fraud;
 
+import com.microservices.clients.fraud.FraudCheckResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
