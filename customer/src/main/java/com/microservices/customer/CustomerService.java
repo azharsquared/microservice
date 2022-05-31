3b0528f56350d80f0f32d764fe79b87f43caedaa
azharsqaured <azharsquared@gmail.com>
2022-05-31 17:15:46 +0530

    open feign sleuth and zipkin

--- customer/src/main/java/com/microservices/customer/CustomerService.java
+++ customer/src/main/java/com/microservices/customer/CustomerService.java
@@ -1,10 +1,10 @@
 package com.microservices.customer;
 
 import com.microservices.clients.fraud.FraudClient;
+import com.microservices.clients.notification.NotificationClient;
+import com.microservices.clients.notification.NotificationRequest;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 @AllArgsConstructor
@@ -13,6 +13,7 @@ public class CustomerService {
     private final CustomerRepository customerRepository;
     private final FraudClient fraudClient;
     //private final RestTemplate restTemplate;
+    private final NotificationClient notificationClient;
 
     public void registerCustomer(CustomerRegistrationRequest customerRegistrationRequest) {
         Customer customer = Customer.builder().firstName(customerRegistrationRequest.firstName())
@@ -22,5 +23,13 @@ public class CustomerService {
         customerRepository.saveAndFlush(customer);
        // restTemplate.getForObject("http://FRAUD/api/v1/fraud-check/{customerID}",FraudCheckResponse.class,customer.getId());
         fraudClient.isFraudster(customer.getId());
+        // todo: make it async. i.e add to queue
+        notificationClient.sendNotification(
+                new NotificationRequest(
+                        customer.getId(),
+                        customer.getEmail(),
+                        String.format("Hi %s, you are registered...",
+                                customer.getFirstName())
+                ));
     }
 }
---
+++ notification/src/main/java/com/microservices/notification/NotificationService.java
@@ -0,0 +1,27 @@
+package com.microservices.notification;
+
+import com.microservices.clients.notification.NotificationRequest;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class NotificationService {
+    private final NotificationRepository notificationRepository;
+
+    public void send(NotificationRequest notificationRequest) {
+        notificationRepository.save(
+                Notification.builder()
+                        .toCustomerId(notificationRequest.toCustomerId())
+                        .toCustomerEmail(notificationRequest.toCustomerName())
+                        .sender("microservices")
+                        .message(notificationRequest.message())
+                        .sentAt(LocalDateTime.now())
+                        .build()
+        );
+    }
+}
