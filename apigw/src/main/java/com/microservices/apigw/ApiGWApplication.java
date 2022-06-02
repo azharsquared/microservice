d8bc5e904d8a3985a9674179b77f3b9f6d7f4764
azharsqaured <azharsquared@gmail.com>
2022-06-02 15:02:25 +0530

    API gateway integration

---
+++ apigw/src/main/java/com/microservices/apigw/ApiGWApplication.java
@@ -0,0 +1,15 @@
+package com.microservices.apigw;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@Slf4j
+public class ApiGWApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGWApplication.class,args);
+    }
+}
